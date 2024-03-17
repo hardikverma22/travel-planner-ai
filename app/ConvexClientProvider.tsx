a244baa0d8af88109a8df04b72fbbe90cb821a79
Hardik Verma <hardikverma22@rediffmail.com>
2024-03-17 19:15:44 +0530

    fix: vercel deployement fix due to clerk pkg

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -1,22 +1,14 @@
 "use client";
 import {ReactNode} from "react";
 import {ConvexReactClient} from "convex/react";
-import {ClerkProvider, useAuth} from "@clerk/clerk-react";
+import {ClerkProvider, useAuth} from "@clerk/nextjs";
 import {ConvexProviderWithClerk} from "convex/react-clerk";
 
-const convex = new ConvexReactClient(
-  process.env.NEXT_PUBLIC_CONVEX_URL as string
-);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
-export default function ConvexClientProvider({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function ConvexClientProvider({children}: {children: ReactNode}) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
-    >
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
