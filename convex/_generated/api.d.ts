02d1cc7005a582ec41a0a5c30e93e41817a7c748
Hardik Verma <hardikverma22@rediffmail.com>
2024-04-13 15:45:06 +0530

    deploy: cleanup function

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as cleanup from "../cleanup.js";
 import type * as http from "../http.js";
 import type * as images from "../images.js";
 import type * as payments from "../payments.js";
@@ -31,6 +32,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  cleanup: typeof cleanup;
   http: typeof http;
   images: typeof images;
   payments: typeof payments;
---
+++ convex/cleanup.ts
@@ -0,0 +1,24 @@
+import { api } from "./_generated/api";
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const fixImages = mutation({
+    args: { date: v.number() },
+    handler: async (ctx, args) => {
+
+        const plans = await ctx.db.query("plan")
+            .filter(q => q.gte(q.field("_creationTime"), args.date))
+            .collect();
+
+        plans?.forEach(async plan => {
+            const name = plan.nameoftheplace.split(",")
+            if (name && name.length > 1) {
+                // console.log(name);
+                await ctx.scheduler.runAfter(0, api.images.generateAndStore, {
+                    prompt: name[0],
+                    planId: plan._id
+                });
+            }
+        });
+    },
+});
