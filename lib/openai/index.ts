15d7d41e38a0d8ca082315dfc8cff55f12068887
Hardik Verma <hardikverma22@rediffmail.com>
2024-10-02 16:58:22 +0530

    Changing the deprecated gpt model and using 4o-mini

--- lib/openai/index.ts
+++ lib/openai/index.ts
@@ -16,7 +16,7 @@ const promptSuffix = `generate travel data according to the schema and in json f
 const callOpenAIApi = (prompt: string, schema: any, description: string) => {
   console.log({ prompt, schema });
   return openai.chat.completions.create({
-    model: "gpt-3.5-turbo-0613",
+    model: "gpt-4o-mini",
     messages: [
       { role: "system", content: "You are a helpful travel assistant." },
       { role: "user", content: prompt },
