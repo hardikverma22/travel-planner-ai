ddb782cae03092970cb162addfc553d96513dad8
hardikverma22 <hardikverma22@rediffmail.com>
2025-01-13 21:20:25 +0530

    fix: generating stuck plans

--- convex/cleanup.ts
+++ convex/cleanup.ts
@@ -3,56 +3,90 @@ import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
 export const fixImages = mutation({
-    args: { date: v.number() },
-    handler: async (ctx, args) => {
+  args: { date: v.number() },
+  handler: async (ctx, args) => {
+    const plans = await ctx.db
+      .query("plan")
+      .filter((q) => q.gte(q.field("_creationTime"), args.date))
+      .collect();
 
-        const plans = await ctx.db.query("plan")
-            .filter(q => q.gte(q.field("_creationTime"), args.date))
-            .collect();
-
-        plans?.forEach(async plan => {
-            const name = plan.nameoftheplace.split(",")
-            if (name && name.length > 1) {
-                await ctx.scheduler.runAfter(0, api.images.generateAndStore, {
-                    prompt: name[0],
-                    planId: plan._id
-                });
-            }
+    plans?.forEach(async (plan) => {
+      const name = plan.nameoftheplace.split(",");
+      if (name && name.length > 1) {
+        await ctx.scheduler.runAfter(0, api.images.generateAndStore, {
+          prompt: name[0],
+          planId: plan._id,
         });
-    },
+      }
+    });
+  },
 });
 
-
 export const deleteOrphanImnages = mutation({
-    async handler(ctx, args) {
-        const planStorageIds = (await ctx.db.query("plan").collect()).map(p => p.storageId);
-        const storageIds = (await ctx.db.system.query("_storage").collect()).map(a => a._id);
-        console.log(planStorageIds);
-        console.log(storageIds);
+  async handler(ctx, args) {
+    const planStorageIds = (await ctx.db.query("plan").collect()).map(
+      (p) => p.storageId
+    );
+    const storageIds = (await ctx.db.system.query("_storage").collect()).map(
+      (a) => a._id
+    );
+    console.log(planStorageIds);
+    console.log(storageIds);
 
-        storageIds.forEach(async id => {
-            if (!planStorageIds.includes(id))
-                return await ctx.storage.delete(id);
-        })
-    },
-})
+    storageIds.forEach(async (id) => {
+      if (!planStorageIds.includes(id)) return await ctx.storage.delete(id);
+    });
+  },
+});
 
 export const fillDefaultValues = mutation({
-    async handler(ctx, args) {
-        const planIds = (await ctx.db.query("plan").collect()).map(p => p._id);
-        planIds?.forEach(async planId => {
-            await ctx.db.patch(planId, {
-                contentGenerationState: {
-                    imagination: true,
-                    abouttheplace: true,
-                    adventuresactivitiestodo: true,
-                    topplacestovisit: true,
-                    itinerary: true,
-                    localcuisinerecommendations: true,
-                    packingchecklist: true,
-                    besttimetovisit: true
-                }
-            })
-        });
-    },
-})
+  async handler(ctx, args) {
+    const planIds = (await ctx.db.query("plan").collect()).map((p) => p._id);
+    planIds?.forEach(async (planId) => {
+      await ctx.db.patch(planId, {
+        contentGenerationState: {
+          imagination: true,
+          abouttheplace: true,
+          adventuresactivitiestodo: true,
+          topplacestovisit: true,
+          itinerary: true,
+          localcuisinerecommendations: true,
+          packingchecklist: true,
+          besttimetovisit: true,
+        },
+      });
+    });
+  },
+});
+
+export const fixPlanGenerations = mutation({
+  args: { planIds: v.array(v.id("plan")), shouldFixImages: v.boolean() },
+  handler: async (ctx, args) => {
+    args.planIds?.forEach(async (planId) => {
+      await ctx.scheduler.runAfter(0, api.plan.prepareBatch1, {
+        planId,
+      });
+      await ctx.scheduler.runAfter(0, api.plan.prepareBatch2, {
+        planId,
+      });
+      await ctx.scheduler.runAfter(0, api.plan.prepareBatch3, {
+        planId,
+      });
+    });
+
+    if (args.shouldFixImages) {
+      args.planIds?.forEach(async (planId) => {
+        const plan = await ctx.db.get(planId);
+        if (plan && plan.nameoftheplace.length > 0) {
+          const name = plan.nameoftheplace.split(",");
+          if (name && name.length > 1) {
+            await ctx.scheduler.runAfter(0, api.images.generateAndStore, {
+              prompt: name[0],
+              planId: plan._id,
+            });
+          }
+        }
+      });
+    }
+  },
+});
