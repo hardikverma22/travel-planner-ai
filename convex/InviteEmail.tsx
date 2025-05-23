c3cb7910afcd9c97410829e4ad0e9a289f7fc195
hardikverma22 <hardikverma22@gmail.com>
2025-05-23 21:52:37 +0530

    fixing the domain name in emails

--- convex/InviteEmail.tsx
+++ convex/InviteEmail.tsx
@@ -14,10 +14,16 @@ import {
 } from "@react-email/components";
 import React from "react";
 
-const InviteEmail = ({inviteLink, projectName}: {inviteLink: string; projectName: string}) => {
+const InviteEmail = ({
+  inviteLink,
+  projectName,
+}: {
+  inviteLink: string;
+  projectName: string;
+}) => {
   const previewText = `You've been invited to join ${projectName} on TravelPlannerAI!`;
 
-  const BASE_URL = process.env.HOSTING_URL ?? "https://travelplannerai.online";
+  const BASE_URL = process.env.HOSTING_URL ?? "https://travelplannerai.site";
 
   return (
     <Html>
@@ -28,7 +34,7 @@ const InviteEmail = ({inviteLink, projectName}: {inviteLink: string; projectName
           <Container className="border border-solid border-[#eaeaea] rounded my-[140px] mx-auto p-[20px]">
             <Section className="mt-[32px]">
               <Img
-                src={`https://travelplannerai.online/tarvelplannerai-logo.png`}
+                src={`https://travelplannerai.site/tarvelplannerai-logo.png`}
                 alt="Travel Planner AI logo"
                 width={160}
                 height={48}
@@ -36,11 +42,11 @@ const InviteEmail = ({inviteLink, projectName}: {inviteLink: string; projectName
             </Section>
             <Section className="text-center mt-[32px] mb-[32px]">
               <Text className="text-black font-medium text-[14px] leading-[24px]">
-                You&apos;ve been invited to join <b>{projectName}</b> Travel Plan on Travel Planner
-                AI
+                You&apos;ve been invited to join <b>{projectName}</b> Travel
+                Plan on Travel Planner AI
               </Text>
               <Button
-                style={{color: "white !important"}}
+                style={{ color: "white !important" }}
                 className="bg-[#3b82f6] rounded text-[#ffffff]
                            text-[12px] font-semibold no-underline text-center p-3"
                 href={inviteLink}
@@ -48,10 +54,15 @@ const InviteEmail = ({inviteLink, projectName}: {inviteLink: string; projectName
                 Join the Plan
               </Button>
               <Text className="text-black text-[14px] leading-[24px]">
-                If you have recieved this invite in error, you may safely ignore this.
+                If you have recieved this invite in error, you may safely ignore
+                this.
               </Text>
               <Text className="text-black font-medium text-[14px] leading-[24px]">
-                <Link href={BASE_URL} target="_blank" className="text-[#2754C5] underline">
+                <Link
+                  href={BASE_URL}
+                  target="_blank"
+                  className="text-[#2754C5] underline"
+                >
                   Get Started
                 </Link>{" "}
                 with Travel Planner AI.
